2737cbb5c251959ea8e6151eb3e493185aedf17a
Clinton <clintonf@gmail.com>
2020-01-11 16:48:15 -0800

    User can specify the dataset they want and can restrict it to a neighbourhood as defined in the dataset.
    But for some reason a max of 10 records is displayed

--- REST_test.py
+++ REST_test.py
@@ -1,16 +1,26 @@
 import requests
 from requests import RequestException
 
-#Make the REST request GET request
-#Here we're asking for rental properties with outstanding (ie. unresolved) issues
-resp = requests.get('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=rental-standards-current-issues')
+dataset = input("Which dataset do you want (type it exactly)")
+region = input("Specify your neighbourhood (press enter for none)")
+
+# Make the REST request GET request
+# Here we're asking for rental properties with outstanding (ie. unresolved) issues
+if region:
+    resp = requests.get('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=' + dataset,
+                        params={'refine.geo_local_area': region})
+else:
+    resp = requests.get('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=' + dataset)
+
 if resp.status_code != 200:
     # This means something went wrong.
     raise RequestException('GET /tasks/ {}'.format(resp.status_code))
 
-records = resp.json()['records']
+print("You searched for '{}'".format(resp.url))
+print("There are {} records".format(resp.json()['nhits']))
 
-print("There are", len(records), "records")
+# Grab the data records
+records = resp.json()['records']
 
 for k in records:
     print(k)
