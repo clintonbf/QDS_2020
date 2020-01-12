a422335a1b42e1e4ef28827daaf6bd405ef0cfb6
Clinton <clintonf@gmail.com>
2020-01-12 12:08:04 -0800

    Function to get record size.

--- REST_test.py
+++ REST_test.py
@@ -2,8 +2,7 @@ import requests
 from requests import RequestException
 
 #Make the REST request GET request
-#Here we're asking for rental properties with outstanding (ie. unresolved) issues
-resp = requests.get('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=rental-standards-current-issues')
+resp = requests.get('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=schools', {'refine.geo_local_area': 'Kitsilano'})
 if resp.status_code != 200:
     # This means something went wrong.
     raise RequestException('GET /tasks/ {}'.format(resp.status_code))
